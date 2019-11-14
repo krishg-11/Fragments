e55eafdbdb7d6d49c92314ffeb038557e0888930
Krish <krishganotra@gmail.com>
2019-11-14 09:34:17 -0500

    multiple fragments

--- app/src/main/java/traf1/ganotrakrish/fragments/MainActivity.java
+++ app/src/main/java/traf1/ganotrakrish/fragments/MainActivity.java
@@ -1,6 +1,7 @@
 package traf1.ganotrakrish.fragments;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Begin the transaction
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Replace the contents of the container with the new fragment
+        ft.replace(R.id.your_placeholder, new FooFragment());
+        ft.replace(R.id.your_placeholder2, new FooFragment());
+        // or ft.add(R.id.your_placeholder, new FooFragment());
+        // Complete the changes added above
+        ft.commit();
     }
 }
