202b92cbcbe87c24d4ecebaf33ed85ee758283f2
Krish <krishganotra@gmail.com>
2019-12-03 10:14:50 -0500

    RecyclerView in multiple fragments

--- app/src/main/java/traf1/ganotrakrish/fragments/FooFragment.java
+++ app/src/main/java/traf1/ganotrakrish/fragments/FooFragment.java
@@ -6,14 +6,46 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FooFragment extends Fragment {
     // The onCreateView method is called when Fragment should create its View object hierarchy,
     // either dynamically or via XML layout inflation.
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.fragment_foo, parent, false);
+        View view =  inflater.inflate(R.layout.fragment_foo, parent, false);
+
+        recyclerView = view.findViewById(R.id.my_recycler_view);
+
+
+
+
+        // use this setting to
+        // improve performance if you know that changes
+        // in content do not change the layout size
+        // of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new traf1.ganotrakrish.recyclerview.MyAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
     }
 
     // This event is triggered soon after onCreateView().
@@ -22,5 +54,10 @@ public class FooFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Setup any handles to view objects here
         // EditText etFoo = (EditText) view.findViewById(R.id.etFoo);
+
+
     }
+
+
+
 }
